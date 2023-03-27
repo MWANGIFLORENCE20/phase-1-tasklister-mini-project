d8780abab63b09ad6360c5bd5c7f621026d9a3e2
florence wangechi <florencewangechim@gmail.com>
2023-03-27 11:37:02 +0300

    Complete Assingment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let li = document.createElement("li")
+  console.log(li);
+
+const createbutton = handletask => {
+  const butt = document.createElement("button")
+  butt.textContent = "y"
+  console.log(butt)
+
+}})
+
